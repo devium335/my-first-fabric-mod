c84625446b70e71ab496eabc96690b213e99da6c
techpluscraft <techpluscraft@gmail.com>
2022-07-04 15:01:54 -0400

    Custom Items

--- src/main/java/net/techpluscraft/tutorialmod/TutorialMod.java
+++ src/main/java/net/techpluscraft/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.techpluscraft.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.techpluscraft.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
