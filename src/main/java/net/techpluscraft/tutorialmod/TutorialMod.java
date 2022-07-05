3a31ed2f0929da7d2ef7acd9ce1d37209440fbcb
techpluscraft <techpluscraft@gmail.com>
2022-07-05 14:05:20 -0400

    Custom Blocks

--- src/main/java/net/techpluscraft/tutorialmod/TutorialMod.java
+++ src/main/java/net/techpluscraft/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.techpluscraft.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.techpluscraft.tutorialmod.block.ModBlocks;
 import net.techpluscraft.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,5 +13,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
